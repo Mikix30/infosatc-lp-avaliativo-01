a725353c85f41ee04614ca4160267a9af96cb91d
Mikix30 <juliabrunelli@outlook.com>
2020-08-31 14:01:23 -0300

    feat: adiciona exercício 29

---
+++ exercicio29.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Qual o primeiro valor ?"))
+nota2 = float(input("Qual o segundo valor ?"))
+nota3 = float(input("Qual o terceiro valor ?"))
+nota4 = float(input("Qual o quarto valor ?"))
+
+calculo = nota1 + nota2 + nota3 + nota4
+calculo2 = calculo/4
+
+print("A média do aluno é de", calculo2)

201487c8f4335f0eb9930de1054dd7835d540f0b
Mikix30 <juliabrunelli@outlook.com>
2020-08-31 14:01:31 -0300

    feat: adiciona exercício 30

---
+++ exercicio30.py
@@ -0,0 +1,6 @@
+valorreal = float(input("Qual o valor a ser adicionado em reais ?"))
+dolarcot = float(input("Qual a cotação do dólar?"))
+
+calculo = valorreal/dolarcot
+
+print("O valor em Dólares é de", calculo)

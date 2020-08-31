4203e0c0d71bd5087f83b83386fa8425669c2b31
Mikix30 <juliabrunelli@outlook.com>
2020-08-31 13:50:24 -0300

    feat: adiciona exercício 07

---
+++ exercicio07.py
@@ -0,0 +1,7 @@
+Fire = float(input("Qual a temperatura em Farenheit? "))
+
+calculo = 5.0*(Fire-32)/9
+
+print ("A temperatura em C é de ", 5.0*(Fire-32)/9)
+
+#recadinho pra fran,saudade dos tutoriais.
